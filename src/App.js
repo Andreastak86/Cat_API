339faee4a3415627fc569a9ca010337b7e32a860
Andreastak <104064121+Andreastak86@users.noreply.github.com>
2022-09-20 22:01:39 +0200

    comit to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [fact, setFact] = useState("");
+
+  // function getRandomCatFact(fct) {
+  //   const randomFact = Math.floor(Math.random() * fct.length);
+  //   const hardFact = fct[randomFact];
+  //   return hardFact;
+  // }
+  // const results = getRandomCatFact(setFact);
+
+  const getFact = () => {
+    axios.get("https://catfact.ninja/facts").then((response) => {
+      console.log(response.data.data);
+      const listOfFact = response.data.data;
+      setFact(listOfFact[9].fact);
+      console.log(fact);
+      // console.log(results);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> KittyCat Facts</h1>
+      <button onClick={getFact}>Get me som kitty-facts</button>
+      <br></br>
+      <h3>{fact}</h3>
     </div>
   );
 }
